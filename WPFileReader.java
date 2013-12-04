8ae636339157f75d1514cd461c48babba8d32c66
apple-ihack-geek <kc2mnc@gmail.com>
2013-12-04 14:02:22 -0500

    Update WPFileReader.java
    just added some comments

--- WPFileReader.java
+++ WPFileReader.java
@@ -2,27 +2,27 @@ import edu.rit.util.Hex;
 import edu.rit.util.Instance;
 import java.io.*;
 import java.util.Scanner;
-/*Modified version of TimeHashFunction.java from CS lib */
+/*Modified version of TimeHashFunction.java from RIT CS lib */
 public class WPFileReader
 	{
 	public static void main
 		(String[] args)
 		throws Exception
 		{
-		if (args.length != 2) usage();
-		String hashclass = args[0];
+		if (args.length != 2) usage(); //make sure we have the correct number of arguments
+		String hashclass = args[0];   // picks which hash based on parameter
 		HashFunction hash = (HashFunction)
 			Instance.newInstance (hashclass+"()");
-		byte[] digest = new byte [hash.digestSize()];
+		byte[] digest = new byte [hash.digestSize()]; // greats output holder
 		long t1 = System.currentTimeMillis();
-	    Scanner     fileScanner = new Scanner( new File( args[1] ));
-	    while( fileScanner.hasNext()) {
-	         String current      = fileScanner.nextLine();
-	         for (int i=0;i<current.length();i++){
+	    Scanner     fileScanner = new Scanner( new File( args[1] )); // opens the file in a scanner
+	    while( fileScanner.hasNext()) {  			//for each line in the file
+	         String current      = fileScanner.nextLine(); 
+	         for (int i=0;i<current.length();i++){ 
 	        	 //for each char...
 	        	 int q = current.charAt(i);
-	        	 System.out.print(current.charAt(i));
-	        	 hash.hash(q); 
+	        	 System.out.print(current.charAt(i)); //print out chars for verification
+	        	 hash.hash(q); //hash each char in the line
 	         }
 	    }
 	    System.out.println();
